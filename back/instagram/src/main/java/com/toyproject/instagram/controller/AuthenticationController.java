27da9a42702da9331d416e8b9b376c087d798b4c
SeongGwangJu <seonggwangdev@gmail.com>
2023-09-25 12:07:21 +0900

    studing : Signup Validation

--- back/instagram/src/main/java/com/toyproject/instagram/controller/AuthenticationController.java
+++ back/instagram/src/main/java/com/toyproject/instagram/controller/AuthenticationController.java
@@ -4,10 +4,13 @@ import com.toyproject.instagram.dto.SignupReqDto;
 import com.toyproject.instagram.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -17,9 +20,14 @@ public class AccountController {
 	private final UserService userService;
 
 	@PostMapping("/user")
-	public ResponseEntity<?> signup(@RequestBody SignupReqDto signupReqDto) {
+	public ResponseEntity<?> signup(@Valid @RequestBody SignupReqDto signupReqDto, BindingResult bindingResult) {
+
+		if(bindingResult.hasErrors()) {
+			bindingResult.getFieldErrors().forEach(error -> {
+				System.out.println(error.getDefaultMessage());
+			});
+		}
 		userService.signupUser(signupReqDto);
 		return ResponseEntity.ok(null);
-
 	}
 }
