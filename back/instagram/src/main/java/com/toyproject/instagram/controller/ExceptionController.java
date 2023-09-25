50faf869ab0bbc4b15b069069789813c55dabd68
SeongGwangJu <seonggwangdev@gmail.com>
2023-09-25 17:09:43 +0900

    temporary

--- back/instagram/src/main/java/com/toyproject/instagram/controller/ExceptionController.java
+++ back/instagram/src/main/java/com/toyproject/instagram/controller/ExceptionController.java
@@ -1,9 +1,19 @@
 package com.toyproject.instagram.controller;
 
+import exception.SignupException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
 
+
+	//예외처리
+	@ExceptionHandler(SignupException.class)
+	public ResponseEntity<?> SignupExceptionHandle(SignupException signupException) {
+		return ResponseEntity.badRequest().body(signupException);
+
+	}
 }
