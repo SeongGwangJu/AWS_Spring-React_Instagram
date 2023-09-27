f335fae11ad912ee10f161490f4e6cb0b8b983b9
SeongGwangJu <seonggwangdev@gmail.com>
2023-09-27 10:00:25 +0900

    generate JWT and Return

--- back/instagram/src/main/java/com/toyproject/instagram/security/JwtTokenProvider.java
+++ back/instagram/src/main/java/com/toyproject/instagram/security/JwtTokenProvider.java
@@ -1,8 +1,29 @@
 package com.toyproject.instagram.security;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+
+//JWT 토큰을 관리해주는 로직
 @Component
 public class JwtTokenProvider {
 
+	private final Key key;
+	// *AutoWired : IoC 컨테이너에서 객체를 자동 주입
+	// Value는 application.yml에서 변수 데이터를 자동 주입
+
+	//@Value : 컴포넌트가 IoC에서 생성될때 DI가 됨.
+	public JwtTokenProvider(@Value("${jwt.secret}") String secret) {
+		//Key값을 만들어줌
+		key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+	}
+
+	// JWT 토큰을 생성
+	public String generateAccessToken(Authentication authentication) {
+		return  null;
+	}
 }
