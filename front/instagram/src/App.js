37ab4a902ae9118167122403df30d0f7e3d63332
SeongGwangJu <seonggwangdev@gmail.com>
2023-10-10 04:34:03 +0900

    FE : -ed: image upload in feedPage ing: profilePage designing.

--- front/instagram/src/App.js
+++ front/instagram/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import RootLayout from './components/Layouts/RootLayout/RootLayout';
-import Sidebar from './components/Sidebar/Sidebar';
 import { Reset } from 'styled-reset';
-import Signup from './pages/Signup/Signup';
-import Signin from './pages/Signin/Signin';
 import AuthRoute from './auth/AuthRoute';
+import RootLayout from './components/Layouts/RootLayout/RootLayout';
 import Home from './pages/Home/Home';
+import Profile from './pages/Profile/Profile';
+import Signin from './pages/Signin/Signin';
+import Signup from './pages/Signup/Signup';
 
 function App(props) {
   return (
@@ -14,10 +14,10 @@ function App(props) {
       {/* <Sidebar /> */}
       <Reset />
       <Routes>
-        <Route path='/' element={ <AuthRoute element={ <Home />} /> } />
-        <Route path='/accounts/emailsignup' element={ <Signup /> } />
-        <Route path='/accounts/login' element={ <Signin />} />
-        <Route path='/:username' element={<div></div>} />
+      <Route path='/' element={ <AuthRoute element={ <Home /> } /> } />
+        <Route path='/accounts/emailsignup' element={ <AuthRoute element={ <Signup /> }/> } />
+        <Route path='/accounts/login' element={ <AuthRoute element={ <Signin /> }/> } />
+        <Route path='/:username' element={<AuthRoute element={ <Profile /> }/>} />
         <Route path='/explore' element={<div></div>} />
       </Routes>
     </RootLayout>
